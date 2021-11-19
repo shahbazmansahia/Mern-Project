f73e092565cb7b7c199b2edfb313c2e7e7ad52ea
shahbazmansahia <shahbaz_mansahia@yahoo.com>
2021-11-19 01:19:45 -0800

    Inited the actual code files and started developing the practice project.

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,18 +1,22 @@
 const mongoose = require('mongoose');
 const config = require('config');
-const db = config.get ('mongoURI');
-//using async-away protocol instead of promises
-const connectDB = async () => { 
-    try {
-        await mongoose.connect(db)
-        
-        console.log('Database Connection successfully established!');
-    }
-    catch (err){
-        console.log('Database connection failed. Reason of error: /n');
-        console.error(err.message);
-        process.exit(1);
-    }
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true
+    });
+
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.error(err.message);
+    // Exit process with failure
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,31 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
 const connectDB = require('./config/db');
-
-require("dotenv").config();
+const path = require('path');
 
 const app = express();
 
-//Establish DB connection
+// Connect Database
 connectDB();
 
-app.get('/', (req, res) => res.send('API is Running'));
-
-// Defining Route
-app.use('/api/users', require('./routes/api/users.js'));
-const port = process.env.PORT || 5000;
-
-app.use(cors());
+// Init Middleware
 app.use(express.json());
 
-app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
-});
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
