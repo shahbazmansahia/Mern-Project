a97927ea331971d4d0135a97840fdcefa67f864b
shahbazmansahia <shahbaz_mansahia@yahoo.com>
2021-12-13 18:46:59 -0800

    Added the ability to get all posts, get post by ID and delete post by ID.

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -46,4 +46,82 @@ async (req, res) => {
 
 });
 
+
+// @route   GET api/posts
+// @desc    Get all posts
+// @access  Private
+
+router.get('/', [auth],
+async (req, res) => {
+    try {
+        // get all posts and sort them in ascending order by date (we'd do +1/1 for descending order)
+        const posts = await Post.find().sort({ date: -1 });
+        res.json(posts);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send ('Post route: Server Error!');
+    }
+});
+
+
+// @route   GET api/posts/:id
+// @desc    Get post by ID
+// @access  Private
+
+router.get('/:id', [auth],
+async (req, res) => {
+    try {
+        // get post for listed id 
+        const post = await Post.findById(req.params.id);
+        
+        if (!post){
+            return res.status (404).json({ msg: 'Post not found' });
+        }
+        
+        res.json(post);
+    } catch (error) {
+        console.error(error.message);
+        if (error.kind === 'ObjectId'){
+            return res.status (404).json({ msg: 'Post not found' });
+        }
+        
+        res.status(500).send ('Post route: Server Error!');
+    }
+});
+
+
+// @route   DELETE api/posts/:id
+// @desc    Delete a post
+// @access  Private
+
+router.delete('/:id', [auth],
+async (req, res) => {
+    try {
+        // Delete post given ID
+        const post = await Post.findById(req.params.id);
+        
+        if (!post){
+            return res.status (404).json({ msg: 'Post not found' });
+        }
+
+        // Check user creds
+        if(post.user.toString() !== req.user.id){
+            return res.status(401).json({ msg: 'The User does not have permissions to do this.'});
+        }
+        
+        await post.remove();
+
+        res.json({msg: 'Post removed'});
+    } catch (error) {
+        console.error(error.message);
+
+        if (error.kind === 'ObjectId'){
+            return res.status (404).json({ msg: 'Post not found' });
+        }
+        res.status(500).send ('Post route: Server Error!');
+    }
+});
+
+
+
 module.exports = router;
