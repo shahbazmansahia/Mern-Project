a7f7ff0471ec2f9ed67f11cac1e83cc82d6e96d0
shahbazmansahia <shahbaz_mansahia@yahoo.com>
2021-12-15 01:48:38 -0800

    Added functionality to the index page using App.js. Tweaked the Landing and Navbar components/elements to React syntax to make all links functional. Added login and register routes and tweaked them to represent React syntax. Also had to update the syntax to newer versions of react as has been mentioned in the comments (especially mentioned in app.js)

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React, { Fragment } from 'react';
+// Changed Switch to Routes due to incompatibility with the latest version of React
+import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Register from './components/layout/auth/Register';
+import Login from './components/layout/auth/Login';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+/* The <Landing /> tag was changed from 'Landing', component was changed to element due to compatibility issues with the latest version of React*/
+        
+const App = () => (
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Routes>
+        <Route exact path= '/' element = {<Landing />} />
+      </Routes>
+        <section className = "container">
+          <Routes>
+            <Route exact path='/register' element = {<Register />} />
+            <Route exact path='/login' element = {<Login />} />
+          </Routes>
+        
+        </section>
+           
+    </Fragment>
+  
+  </Router>
   );
-}
 
 export default App;
---
+++ client/src/components/layout/auth/Register.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Register = () => {
+    return (
+        <div>
+            Register
+        </div>
+    )
+}
+
+
+export default Register
